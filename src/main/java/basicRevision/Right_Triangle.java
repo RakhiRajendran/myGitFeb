eca5d52685dda7fb1c2598044428646255e6919f
Rakhi <rakhi.rajendran3@gmail.com>
2023-03-09 06:45:39 +0530

    This is feb commit

---
+++ src/main/java/basicRevision/Right_Triangle.java
@@ -0,0 +1,12 @@
+package basicRevision;
+
+public class Right_Triangle {
+	public static void main(String args[]) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*" + " ");
+			}
+			System.out.println("");
+		}
+	}
+}
